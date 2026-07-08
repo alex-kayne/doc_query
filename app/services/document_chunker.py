4bc6b8b3e0d27034873b419b7d2f6ad36e24583a
Kan Alexandr <kan.a@delotech.ru>
2026-07-08 14:09:09 +0300

    Пофиксил overlap

--- app/services/document_chunker.py
+++ app/services/document_chunker.py
@@ -1,66 +1,57 @@
 import re
-from collections.abc import Iterator
-from app.schemas.chunk import ChunkCreate
-from hashlib import sha256
 from collections import deque
+from copy import copy
+from hashlib import sha256
+from typing import Generator
+
+from app.schemas.chunk import ChunkCreate
 
 
 class DocumentChunker:
     chunk_size = 200
-    overlap = 40
-    cur_overlap = 0
-    new_overlap_queue = deque()
-    old_overlap_queue = deque()
+    overlap = 40  # 40 слов
 
     @staticmethod
-    def iter_sentences(normalized_text: str) -> Iterator[str]:
+    def iter_sentences(normalized_text: str) -> Generator[tuple[str, int]]:
         pattern = r"(?<=[.!?])\s+"
 
         for sentence in re.split(pattern, normalized_text):
             sentence = sentence.strip()
             if sentence:
-                yield sentence
-
-    def _modify_overlap(self, sentence: str) -> None:
-        if self.cur_overlap < self.overlap:
-            self.cur_overlap += len(sentence)
-        else:
-            self.cur_overlap -= len(self.overlap_queue.popleft())
-        self.overlap_queue.append(sentence)
-
-    def _add_chunk(self, token_count: int, text: str, chunk_index: int,
-                   chunks: list) -> None:
-        if chunk_index == 1:
-            chunk_text = text
-        else:
-            chunk_text = " ".join(self.old_overlap_queue) + text
-        self.old_overlap_queue = self.new_overlap_queue.copy()
-        chunks.append(
-            ChunkCreate(chunk_index=chunk_index,
-                        chunk_text=chunk_text,
-                        chunk_hash=sha256(chunk_text.encode()).hexdigest(),
-                        token_count=token_count))
-        return None
+                yield sentence, sentence.count(" ") + 1
 
     def chunk(self, normalized_text: str) -> list[ChunkCreate]:
         chunks = []
         chunk_index = 1
-        token_count = 0
         text = ""
+        chunk_size = 0
+        overlap_size = 0
+        overlap_queue = deque()
+        overlap_to_add = None
 
-        for sentence in self.iter_sentences(normalized_text):
-            if token_count > self.chunk_size:
-                self._add_chunk(token_count, text, chunk_index, chunks)
+        for sentence, sentence_len in self.iter_sentences(normalized_text):
+            text = text + sentence
+            chunk_size += sentence_len
+            if overlap_size < self.overlap:
+                overlap_size += sentence_len
+            else:
+                del_sentence, del_sentence_len = overlap_queue.popleft()
+                overlap_size -= del_sentence_len + sentence_len
+
+            overlap_queue.append((sentence, sentence_len,))
+
+            if chunk_size > self.chunk_size:
+                if chunk_index != 1:
+                    chunk_size += overlap_size
+                    text = " ".join([seq for seq, _ in overlap_to_add]) + " " + text
+
+                chunks.append(ChunkCreate(chunk_index=chunk_index,
+                                          chunk_text=text,
+                                          chunk_hash=sha256(text.encode()).hexdigest(),
+                                          token_count=chunk_size))
                 chunk_index += 1
+                chunk_size = 0
                 text = ""
-                self.cur_overlap = 0
-                self.overlap_queue = deque()
-                token_count = 0
-
-            token_count += len(sentence.split())
-            text += f"{sentence} "
-            self._modify_overlap(sentence)
-
-        self._add_chunk(token_count, text, chunk_index, chunks)
+                overlap_to_add = copy(overlap_queue)
 
         return chunks
